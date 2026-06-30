5972b29fccaad07d8d2d9e0361b8285d8412f2ab
mayconct32 <maycon326754@gmail.com>
2026-06-30 19:37:49 -0300

    feat(AIGeminiService): implements functionality

--- src/chess_ai/main.py
+++ src/chess_ai/main.py
@@ -1,5 +1,16 @@
 from abc import ABC, abstractmethod
 from typing import Iterator
+from os import getenv
+
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+
+load_dotenv()
+
+
+class APIKeyNotFoundError(Exception):
+    pass
 
 
 class AIModelService(ABC):
@@ -9,18 +20,37 @@ class AIModelService(ABC):
 
 
 class AIGeminiService(AIModelService):
-    def __init__(self) -> None:
-        pass
-    
-    def _connect(self):
-        pass
+    def __init__(self, api_key: str, gemini_version: str) -> None:
+        self._api_key = api_key
+        if not self._api_key:
+            raise APIKeyNotFoundError(
+                "API_KEY not found. Please set it in your .env file."
+                )
+        self.gemini_version = gemini_version
+        self.connection = None
+
+    def _connect(self) -> ChatGoogleGenerativeAI:
+        model = ChatGoogleGenerativeAI(
+            model=self.gemini_version,
+            google_api_key=self._api_key
+        )
+        return model
 
     def request(self, prompt: str) -> Iterator[str]:
-        pass
+        if not self.connection:
+            self.connection = self._connect()
+        for chunk in self.connection.stream(prompt):
+            if chunk.content:
+                yield chunk.content[0]["text"]
 
 
 def main():
-    pass
+    API_KEY = getenv("API_KEY")
+    GEMINI_VERSION = "gemini-3.1-flash-lite"
+    gemini_service = AIGeminiService(API_KEY, GEMINI_VERSION)
+    for chunk in gemini_service.request("olá, Chat. Meu nome é magal."):
+        print(chunk, end="")
+    print()
 
 
 if __name__ == "__main__":

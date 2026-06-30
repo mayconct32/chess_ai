6d2755aaf7b5091b6d21fc7f3a379d115eb8a22f
mayconct32 <maycon326754@gmail.com>
2026-06-30 19:15:10 -0300

    feat: Add the 'AIGeminiService' class.

--- src/chess_ai/main.py
+++ src/chess_ai/main.py
@@ -1,3 +1,24 @@
+from abc import ABC, abstractmethod
+from typing import Iterator
+
+
+class AIModelService(ABC):
+    @abstractmethod
+    def request(self, prompt: str) -> Iterator[str]:
+        pass
+
+
+class AIGeminiService(AIModelService):
+    def __init__(self) -> None:
+        pass
+    
+    def _connect(self):
+        pass
+
+    def request(self, prompt: str) -> Iterator[str]:
+        pass
+
+
 def main():
     pass
 
